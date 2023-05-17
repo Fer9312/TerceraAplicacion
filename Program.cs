3787cc8db45a1f0b68f0d38a0fc4c6e5e08eb600
abc <abc@example.com>
2023-05-16 19:59:18 -0600

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace TerceraAplicacion
+{
+    class Program
+    { 
+    static void Main(string[] args)
+    {
+            //int edadPersona1;
+            //int edadPersona2;
+            //int edadPersona3;
+            //int edadPersona4;
+
+            //edadPersona1 = edadPersona2 = edadPersona3 = edadPersona4 = 27;
+
+            //Console.Write(edadPersona4);
+
+
+
+
+            //int edadPersona1, edadPersona2, edadPersona3, edadPersona4 = 27;
+
+
+            //edadPersona2 = 35;
+
+            //Console.WriteLine(edadPersona2);
+
+
+
+            //Conversion explícita
+
+            double temperatura = 34.9;
+
+            int temperaturaMexico;
+
+            temperaturaMexico = (int) temperatura;
+
+            //Conversion implicita 
+
+            int habitantesCiudad = 1000000000;
+
+            long habitantesCiudad2023 = habitantesCiudad;
+
+
+            float pesoMaterial = 5.78F;
+
+            double pesoMaterialPrec = pesoMaterial;
+
+            Console.WriteLine(pesoMaterialPrec);
+            
+
+
+
+
+
+
+
+        }
+    }
+
+}
+
